7bb6be747b98229a2a5e9e4dd4bbd4d8e2f3d35f
Jesús Jiménez Herrera <jjimenez@profile.es>
2019-12-10 19:09:36 +0100

    POC of conditional properties

---
+++ cloud-storage-spring-java/src/main/java/com/github/sevtech/cloud/storage/spring/annotattions/ConditionalOnCloudStorageProperty.java
@@ -0,0 +1,40 @@
+package com.github.sevtech.cloud.storage.spring.annotattions;
+
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.ConfigurationCondition;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Map;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Conditional(OnPropertyCondition.class)
+public @interface ConditionalOnCloudStorageProperty {
+
+    String value();
+
+    boolean on() default true;
+}
+
+class OnPropertyCondition implements ConfigurationCondition {
+
+    @Override
+    public boolean matches(final ConditionContext context, final AnnotatedTypeMetadata metadata) {
+        final Map attributes = metadata.getAnnotationAttributes(ConditionalOnCloudStorageProperty.class.getName());
+        final String propertyName = (String) attributes.get("value");
+        final boolean propertyDesiredValue = (boolean) attributes.get("on");
+        Boolean condition = context.getEnvironment().getProperty(propertyName, Boolean.class);
+        
+        return condition != null && condition == propertyDesiredValue;
+    }
+
+    @Override
+    public ConfigurationPhase getConfigurationPhase() {
+        return ConfigurationPhase.REGISTER_BEAN;
+    }
+}
